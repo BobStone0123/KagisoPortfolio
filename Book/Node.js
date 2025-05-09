d80db8df902bb255d0255aa6a1e718905a0e2cd5
KagisoMokgobi0123 <162275760+KagisoMokgobi0123@users.noreply.github.com>
2025-05-09 12:53:09 +0200

    Update Node.js

--- Book/Node.js
+++ Book/Node.js
@@ -7,8 +7,9 @@ document.getElementById('contact-form').addEventListener('submit', function(e) {
 
     emailjs.sendForm('service_0i8y748', 'template_iwde5gf', this)
         .then(function(response) {
-           alert('Message sent successfully!');
+            alert('✅ Message sent successfully!');
+            document.getElementById('contact-form').reset();
         }, function(error) {
-           alert('Failed to send message: ' + JSON.stringify(error));
+            alert('❌ Failed to send message. Error: ' + JSON.stringify(error));
         });
 });
